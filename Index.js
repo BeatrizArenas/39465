4481e08c807f794a3ee57f5343f4585c18149e3a
BeatrizArenas <120799036+BeatrizArenas@users.noreply.github.com>
2023-03-17 20:10:27 -0600

    Update Index.js
    Buenas tardes:
    
    Esta es mi segunda entrega del curso de Js, en la cual en primer lugar agregue según las observaciones de mi tutor .toLowerCase, para la validación de mayúsculas y minúsculas, así mismo se le agrego la opción de seguir agregando productos en el carrito y el total de los productos seleccionados.
    
    Saludos

--- Index.js
+++ Index.js
@@ -1,5 +1,3 @@
-///Beatriz Arenas 03-03-2023 
-///Primera entrega del proyecto  
 /// index.js
 // Este codigo implementa la simulación de un carrito de compras
 // para el sitio de gatotips.net
@@ -36,74 +34,102 @@ let carrito = [];
 /// funciones
 
 function iniciarCarrito(){
-    let respuesta1 = prompt("Estas en el carrito de compras, ¿Deseas comprar algo ? si o no:")
+    let respuesta1 = prompt("Estas en el carrito de compras, ¿Deseas comprar algo ? si o no:");
     
-    if (respuesta1 == "si"){
-        // mostrar tipos de productos
-        let respuesta2 = prompt("Estos son los tipos de productos que tenemos:\n (1) Comida\n (2)Higiene\n (3)Juguetes\n Ingresa el número de tu interés");
 
-        mostrarProductos(respuesta2);    
+    //Convertir a minusculas
+    respuesta1 = respuesta1.toLowerCase();
+    let continuar = "si";
+
+    if (respuesta1 == "si"){
+        //ejectua mientras continuar sea "si"
+        while(continuar == "si"){
+            // mostrar tipos de productos
+            let respuesta2 = prompt("Estos son los tipos de productos que tenemos:\n" + 
+            " (1) Comida\n"+
+            " (2)Higiene\n"+
+            " (3)Juguetes\n"+
+            " Ingresa el número de tu interés");
+
+            continuar = mostrarProductos(respuesta2);  
+        }
+          
     };
     
     alert ("Gracias por tu visita");
 };
 
 function mostrarProductos(tipoProducto){
-    //mostrar productos de acuerdo a la categoria
-    switch(tipoProducto){
-        case "1":
-            {
-                // mostrar productos de la categoria
-            let strProductos = "Los productos de esta categoría son estos:\n";
+    let continuarComprando = "si";
 
-            for(let i = 0; i < comida.length; i++){
-                strProductos += "(" + (i+1) + ") " + comida[i].producto + " $" + comida[i].precio + "\n";
-            }
-            
-            let respuesta3 = prompt(strProductos);
-            let intproducto = parseInt (respuesta3);
-            let precio = comida [intproducto-1].precio;
-            mostrarTotalDelCarrito(precio);
-            }
-            
+        //mostrar productos de acuerdo a la categoria
+        switch(tipoProducto){
+            case "1":
+                {
+                    // mostrar productos de la categoria
+                    let strProductos = "Los productos de esta categoría son estos:\n";
+
+                    for(let i = 0; i < comida.length; i++){
+                        strProductos += "(" + (i+1) + ") " + comida[i].producto + " $" + comida[i].precio + "\n";
+                    }
+                    
+                    let respuesta3 = prompt(strProductos);
+                    let intproducto = parseInt (respuesta3);
+                    carrito.push( comida [intproducto-1]);
+                    continuarComprando = mostrarCarrito();
+                }
+                
+                break;
+            case "2":
+                {
+                    let strProductos = "Los productos de esta categoría son estos:\n";
+                for(let i = 0; i < higiene.length; i++){
+                    strProductos += "(" + (i+1) + ") " + higiene[i].producto + " $" + higiene[i].precio + "\n";
+                }
+                let respuesta4 = prompt(strProductos);
+                let intproducto = parseInt (respuesta4);
+                carrito.push(higiene [intproducto-1]);
+                continuarComprando = mostrarCarrito();
+                }
+                
             break;
-        case "2":
-            {
+            case "3":
+                {
                 let strProductos = "Los productos de esta categoría son estos:\n";
-            for(let i = 0; i < higiene.length; i++){
-                strProductos += "(" + (i+1) + ") " + higiene[i].producto + " $" + higiene[i].precio + "\n";
-            }
-            let respuesta4 = prompt(strProductos);
-            let intproducto = parseInt (respuesta4);
-            let precio = higiene [intproducto-1].precio;
-            mostrarTotalDelCarrito(precio);
+                for(let i = 0; i < juguetes.length; i++){
+                    strProductos += "(" + (i+1) + ") " + juguetes[i].producto + " $" + juguetes[i].precio + "\n";
+                }
+                let respuesta5 = prompt(strProductos);
+                let intproducto = parseInt (respuesta5);
+                carrito.push(juguetes [intproducto-1]);
+                continuarComprando = mostrarCarrito();
             }
             
-          break;
-        case "3":
-            {
-            let strProductos = "Los productos de esta categoría son estos:\n";
-            for(let i = 0; i < juguetes.length; i++){
-                strProductos += "(" + (i+1) + ") " + juguetes[i].producto + " $" + juguetes[i].precio + "\n";
-            }
-            let respuesta5 = prompt(strProductos);
-            let intproducto = parseInt (respuesta5);
-            let precio = juguetes [intproducto-1].precio;
-            mostrarTotalDelCarrito(precio);
-        }
-          
-            break;
-        default:
-            alert("Tu selección no es válida");
-            break;
-    };
+                break;
+            default:
+                alert("Tu selección no es válida");
+                break;
+        };
+    return continuarComprando;
 };
 
-/// muestra el total del carrito
-function mostrarTotalDelCarrito(precio)
-{
-alert("El costo de tu producto es: " + precio);
+// mostrar articulos en el carrito
+function mostrarCarrito(){
+    let strCarrito = "Los productos en tu carrito son:\n";
+    let totalCarrito = 0;
+    for(let i = 0; i < carrito.length; i++){
+        strCarrito += carrito[i].producto + " : $" + carrito[i].precio + "\n";
+        totalCarrito += carrito[i].precio;
+    }
+
+    strCarrito += "total del carrito: $" + totalCarrito + "\n\n";
+    strCarrito += "¿Deseas agregar algo más a tu carrito? (si/no)";
+    let respuesta6 = prompt(strCarrito);
+    respuesta6 = respuesta6.toLowerCase();
+
+    return respuesta6;
 }
+
 
 /// eventos
 
